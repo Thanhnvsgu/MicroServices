14966966010a9d0b9eb155cf05654f788c4ddcef
ThahEtn <fifaclubfootball2014@gmail.com>
2019-12-08 16:38:25 +0700

    update!!!

---
+++ build_dockerfile.bat
@@ -0,0 +1,3 @@
+docker build ./dotnet -t "iis_demo"
+docker build ./nodejs -t "iisnode_demo"
+echo build dockerfile complete
---
+++ docker-compose.bat
@@ -0,0 +1,5 @@
+build_dockerfile.bat
+REM docker-compose down
+REM docker network prune
+REM y
+REM docker-compose up
